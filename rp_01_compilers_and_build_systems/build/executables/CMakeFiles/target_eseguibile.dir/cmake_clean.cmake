15df020f6c7010b848b5983917213ba1f3080c9f
Richard-03 <gitSapP4ss>
2025-09-28 11:16:13 +0200

    filled #todos in exercise to understand CMake

---
+++ rp_01_compilers_and_build_systems/build/executables/CMakeFiles/target_eseguibile.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/target_eseguibile.dir/many_object_in_stack_example.cpp.o"
+  "CMakeFiles/target_eseguibile.dir/many_object_in_stack_example.cpp.o.d"
+  "target_eseguibile"
+  "target_eseguibile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/target_eseguibile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
